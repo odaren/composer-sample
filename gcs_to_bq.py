3e94163e05d93998353e5bdc93898bc0d730fcc3
odaren <55085752+odaren@users.noreply.github.com>
2022-03-16 03:26:50 +0900

    Create gcs_to_bq.py

---
+++ gcs_to_bq.py
@@ -0,0 +1,39 @@
+import datetime
+
+import airflow
+from airflow import models
+from airflow.contrib.operators import bigquery_operator
+from airflow.contrib.operators import gcs_to_bq
+
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': YESTERDAY,
+}
+
+with models.DAG(
+    dag_id='gcs_to_bq_sample',
+    default_args=default_args,
+    schedule_interval=None # スケジュール例'0 * * * *'
+    ) as dag:
+
+    # create dataset
+    create_dataset = bigquery_operator.BigQueryCreateEmptyDatasetOperator(
+        task_id='create_dataset',
+        dataset_id='composer_test_dataset',
+        dag=dag
+    )
+
+    # load csv data from gcs to bigquery
+    load_csv = gcs_to_bq.GoogleCloudStorageToBigQueryOperator(
+        task_id='load_csv',
+        bucket='odaira-composer-test',
+        source_objects=['sample_data.csv'],
+        destination_project_dataset_table='tcomposer_test_dataset.composer_test_table',
+        write_disposition='WRITE_TRUNCATE',
+        autodetect=True,
+        dag=dag
+    )
+
+create_dataset >> load_csv
